894368184bd27112b719d824401c9bfd83fb971b
Butch Bet <butchbet01@gmail.com>
2023-05-01 13:18:58 -0500

    Created the Person class.

---
+++ src/module/Person.java
@@ -0,0 +1,25 @@
+package module;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private long phoneNumber;
+
+    public Person(String firstName, String lastName, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+}
