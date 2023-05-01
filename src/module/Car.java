aa02a1c70bcd03b71acf53be0c7a6abd4a7bbc48
Butch Bet <butchbet01@gmail.com>
2023-05-01 13:16:40 -0500

    Created the Car class, the Home screen and started the Requests Screen.

---
+++ src/module/Car.java
@@ -0,0 +1,26 @@
+package module;
+
+public class Car {
+    private String make;
+    private int model;
+    private String plate;
+
+    public Car(String make, int model, String plate) {
+        this.make = make;
+        this.model = model;
+        this.plate = plate;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+    
+}
