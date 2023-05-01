3ca3221fac83a7e06352f19d69eeb4962b181446
Butch Bet <butchbet01@gmail.com>
2023-05-01 14:59:57 -0500

    Added the services into the Request

--- src/module/Request.java
+++ src/module/Request.java
@@ -6,13 +6,14 @@ public class Request {
     private Person owner;
     private Car car;
     private int state; /*-1 Pending, 0 In process, 1 Finished*/
-    private Services petitions;
+    private Services services;
    
 
-    public Request(Person owner, Car car, int state) {
+    public Request(Person owner, Car car, int state, Services services) {
         this.owner = owner;
         this.car = car;
         this.state = state;
+        this.services = services;
     }
 
     public Person getOwner() {
@@ -30,4 +31,8 @@ public class Request {
     public void setState(int state) {
         this.state = state;
     }  
+
+    public Services getServices() {
+        return services;
+    }
 }
