e23660b30b66e8793a108a25cbf25f99c3b50283
Butch Bet <butchbet01@gmail.com>
2023-05-01 13:21:47 -0500

    Created the Request class.

--- src/module/Request.java
+++ src/module/Request.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package module;
 
-/**
- *
- * @author butch
- */
 public class Request {
-    
+    private Person owner;
+    private Car car;
+    private int state; /*-1 Pending, 0 In process, 1 Finished*/
+
+    public Request(Person owner, Car car, int state) {
+        this.owner = owner;
+        this.car = car;
+        this.state = state;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }  
 }
